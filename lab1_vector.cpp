0cfe57561b7023a228574207098b19bcf02fd11d
maryapple <maria9ku@gmail.com>
2020-01-19 19:48:55 +0300

    added lab1

---
+++ lab1_vector.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+#include <vector>
+#include <cstdlib>
+#include <math.h>
+
+using namespace std;
+
+int main()
+{
+    srand(time(0));
+    
+    int N = 0, amountMult3 = 0;
+    vector <float> v;
+    
+    cin >> N;
+    
+    for (int i = 0; i < N; i++) {
+        v.push_back((rand() % 201 - 100) / 10.0);
+    }
+    
+    for (auto i = v.begin(); i != v.end(); i++) {
+        cout << *i << " ";
+        //Проверка делится ли элемент массива на 3
+        if (!fmod(abs(*i), 3)) {
+            amountMult3++;
+        }
+    }
+    cout << "\namountMult3: " << amountMult3 << endl;
+    
+    // Запоминаем последний элемент
+    int temp = v.back();  
+    // cout << temp;
+    
+    for (auto i = v.end(); i > v.begin(); i++) {
+        *i = *(i - 1);
+    }
+    
+    // Записываем в 0 элемент temp
+    // v.at(0) = temp;
+    
+    for (auto i = v.begin(); i != v.end(); i++) {
+        cout << *i << " ";
+    }
+    
+    return 0;
+}
