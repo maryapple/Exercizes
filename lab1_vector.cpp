a006bc761198aaa60f8266a3411b3b4a503cd3c6
maryapple <maria9ku@gmail.com>
2020-02-02 23:26:57 +0300

    Program with iterator finished

--- lab1_vector.cpp
+++ lab1_vector.cpp
@@ -2,87 +2,132 @@
 #include <vector>
 #include <cstdlib>
 #include <math.h>
-<<<<<<< HEAD
-#include <time.h>
-=======
 #include <algorithm>
->>>>>>> a6bff102b78a5253594bb12dc900a7f128b3a5d7
+#include <time.h>
 
 using namespace std;
 
+int printArray (vector <int>v) 
+{
+    for (auto i = v.begin(); i != v.end(); i++)
+        cout << *i << " ";
+}
+
 int main()
 {
-    srand(time(0));
-    
     int N = 0, amountMult3 = 0, amountOfEven = 0;
-    // auto *i1, *i2;
     int sumOfEven = 0;
     vector <int> v;
-        // auto i1 = v.begin(), i2 = v.begin();
-    // cout << "v.begin(): " << &i1 << endl;
     cout << "Enter N: ";
     cin >> N;
-    for (int i = 0; i < N; i++) {
+    
+    // 1. Заполнение массива случайными числами
+    srand(time(0));
+    for (int i = 0; i < N; i++)
         v.push_back((rand() % 201 - 100) );
-    }
+    printArray(v);
 
-    auto first_even = find_if(v.begin(), v.end(), [](int i){ return i % 2 == 0; });
-    auto le = find_if(v.rbegin(), v.rend(), [](int i){ return i % 2 == 0; });
-    vector<int>::iterator last_even = le;
-    cout << "first even: " << *first_even << " last: " << *last_even << endl;
-    
-    for (auto i = v.begin(); i != v.end(); i++) {
-        cout << *i << " ";
-        //Проверка делится ли элемент массива на 3
-        if (! (abs(*i) % 3)) {
+    // 2. Находим, сколько элементов делится на 3 и сумму четных чисел
+    for (auto i = v.begin(); i != v.end(); i++) 
+    {
+        if (! (abs(*i) % 3))
             amountMult3++;
-        }
         
-        // Суммируем четное число
-        if (!(abs(*i)% 2)) {
-/*            amountOfEven ++;
-            if (amountOfEven == 1) {
-                cout << "\nFIRST EVEN " << &i << endl;
-                i1 = i;
-                cout << "i1: " << &i1 << endl;
-            }
-            cout << "even!" << " "; */
+        if (!(abs(*i)% 2))
             sumOfEven += *i;
-        }
     }
-    cout << "\namountMult3: " << amountMult3 << endl;
+    cout << "\n2. Amount of numbers divisible by 3: " << amountMult3 << endl;
     
+    // 3. Циклический сдвиг массива вправо
     // Запоминаем последний элемент
     int temp = v.back();
-    
-    for (auto i = v.end() - 1; i > v.begin(); i--) {
-        // cout << *(i) << " " << *(i - 1) << endl;
+    for (auto i = v.end() - 1; i > v.begin(); i--)
         *i = *(i - 1);
-    }
-    
     // Записываем в 0 элемент temp
     v.at(0) = temp;
+    cout << "3. Right shift: " << endl;
+    printArray(v);
     
-    cout << "Right shift: " << endl;
-    
-    for (auto i = v.begin(); i != v.end(); i++) {
-        cout << *i << " ";
-    }
-    
-    cout << "\nAdded sumOfEven: " << endl;
+    // 4. Добавим в конец массива элемент, равный сумме всех четных элементов массива
+    cout << "\n4. Added sum of all even elements in the end: " << endl;
     v.push_back(sumOfEven);
-    
-    for (auto i = v.begin(); i != v.end(); i++) {
-        cout << *i << " ";
-    }
-    cout << endl;
+    printArray(v);
 
-    // Умножим на 2 
-    for (auto i = last_even; i != v.end(); i++ )
-	{ 
-		*i *= 2;
-		cout << *i << " " ;
-	}
+    // 5. Поиск первого и последнего четных значений
+    vector<int>::iterator first = v.begin() - 1, last = v.begin() - 2;
+    for (auto i = v.begin(); i != v.end(); i++) 
+        if (!(*i % 2)) 
+        {
+            if (first == v.begin() - 1)
+                first = i;
+            last = i;
+        }
+    cout << "\n5. First even element: " << *first << ", last even element: " << *last << endl;
+
+    // Умножение на 2 все элементов до первого и после последнего четных элементов
+    for (auto i = last; i != v.end(); i++) {
+        *i *= 2;
+    }
+
+    for (auto i = first; i != v.begin() - 1; i--) {
+        *i *= 2;
+    }
+    cout << "Array after multiplying by 2:" << endl;
+    printArray(v);
+
+    // 6. Если сумма элементов массива с индексами от i1 до i2 положительная, то удалим из массива все эти элементы
+    int sumBetween = 0;
+    int i1, i2;
+    cout << "\n6. Enter i1 and i2:";
+    cin >> i1 >> i2;
+    for (auto i = v.begin() + i1; i != v.begin() + i2 + 1; i++) 
+        sumBetween += *i;
+
+    if (sumBetween > 0) 
+        v.erase(v.begin() + i1, v.begin() + i2 + 1);
+    cout << "Array after erasing elements between i1 and i2:" << endl;
+    printArray(v);
+
+
+    // 7. Отсортировать по убыванию часть массива от минимального и максимального элементов
+    vector<int>::iterator minEl = v.begin(), maxEl = v.begin();
+
+    for (auto i = v.begin(); i != v.end(); i++) 
+    {
+        if (*i < *minEl) 
+            minEl = i;
+        if (*i > *maxEl)
+            maxEl = i;
+    }
+    cout << "\n7. Sort elements between min=" << *minEl << " and max=" << *maxEl << ": " << endl;
+    auto minIndex = minEl;
+    auto maxIndex = maxEl;
+    if (maxEl < minEl) 
+    {
+        minIndex = maxEl;
+        maxIndex = minEl;
+    }
+    bool swapp = true;
+    while(swapp)
+    {
+        swapp = false;
+        for (auto i = minIndex; i != maxIndex ; i++)
+            if (*i < *(i + 1))
+            {
+                int tmp = *i;
+                *i = *(i + 1);
+                *(i + 1) = tmp;
+                swapp = true;
+            }
+    }
+    printArray(v);
+
+    // 8. Подсчитать сумму элементов массива, кратных 3 или 4
+    int sumOfDiv3_4 = 0;
+    for (auto i = v.begin(); i != v.end(); i++) 
+        if (!(*i % 3) || !(*i % 4)) 
+            sumOfDiv3_4 += *i;
+    cout << "\n8. Sum of elements divisible by 3 or 4: " << sumOfDiv3_4;
     
     return 0;
 }
